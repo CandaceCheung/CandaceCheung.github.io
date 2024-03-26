912899aa4bd3b96f0fb3ddde340f86e890286432
Candace <candacecsy@gmail.com>
2024-03-26 13:30:26 -0400

    delete extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/Home";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <>
-                    <nav>
-                      <Link to="/">Home</Link>
-                      <Link to="/about">Anout Me</Link>
-                    </nav>
-                    <Routes>
-                      <Route path="/" element={<Home/>} />
-                      
-                    </Routes>
+
                 </>
             </BrowserRouter>
         </div>
