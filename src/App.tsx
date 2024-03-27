41b6c839c824a666cbda1836b34ca2f069fde568
Candace <candacecsy@gmail.com>
2024-03-27 19:27:49 -0400

    adding header in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/Header";
+import Footer from "./components/Footer";
 
 // function App() {
 //     return (
@@ -18,7 +20,9 @@ import "./App.css";
 function App(): JSX.Element{
     return(
         <div className="App">
-
+            <Header />
+            
+            <Footer />
         </div>
     )
 }
