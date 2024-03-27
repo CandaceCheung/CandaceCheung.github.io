04192c841be5ca5daa6978281315edf09bde857b
Candace <candacecsy@gmail.com>
2024-03-27 11:49:56 -0400

    added functions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,35 @@
 import styles from "./styles/Header.module.css";
 
-export function Header (){
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
+export function Header(): JSX.Element {
+    const [clicked, setClicked] = useState(false);
+    const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
+    const handleClick = () => {
+        setClicked((current) => {
+            return !current;
+        });
+    };
+
+    const linkClick = () => {
+        setClicked(false)
+    };
 
     return(
-        <div>
-            
-        </div>
+        <header className={styles.header}>
+            <div className={`container ${styles.headerContainer}`}>
+                <div style={{ cursor: "pointer" }}>
+                    {/* <img src={ } alt="" /> */}
+                </div>
+                {(!isMobile || clicked) && (
+                    <nav id="topnav" className={styles.nav}>
+
+                    </nav>
+                )}
+            </div>
+        </header >
     )
 }
