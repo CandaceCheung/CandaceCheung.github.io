827292c6955a92a4bd63ebe347380fb2a7525719
Candace <candacecsy@gmail.com>
2024-03-27 17:35:26 -0400

    still working on the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import { Outlet } from "react-router";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <>
+// function App() {
+//     return (
+//         <div className="App">
+//             <BrowserRouter>
+//                 <>
 
-                </>
-            </BrowserRouter>
+//                 </>
+//             </BrowserRouter>
+//         </div>
+//     );
+// }
+
+
+function App(): JSX.Element{
+    return(
+        <div className="App">
+
         </div>
-    );
+    )
 }
 
 export default App;
