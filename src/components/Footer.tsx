3fd8dd4d8be1493315a92346e7e39b061dd6d286
Candace <candacecsy@gmail.com>
2024-04-16 10:31:46 -0400

    delete file

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles/Footer.module.css";
-
-export default function Footer(): JSX.Element {
-    return (
-        <footer>
-
-        </footer>
-    )
-}
--- src/pages/error/NotFound.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function NotFound(): JSX.Element {
-    useEffect(() => {
-        document.title = "Portfolio | Error";
-    },[]);
-    return <h1>404 Page Not Found</h1>
-}
